4470c6a2a1d1a80df25f49b820b1ebaf8e555885
Hitesh Mishra <85689306+hiteshmishra5@users.noreply.github.com>
2021-07-23 10:36:41 +0530

    Update Number-Guessing-Game.py

--- Number-Guessing-Game.py
+++ Number-Guessing-Game.py
@@ -1,14 +1,18 @@
 import random
 n = random.randint(10,100)
+count = 0
 guess = int(input('Enter a number b/w 1-100:'))
 while n != guess:
     if guess < n:
-        print("Too Low")
+        print("Sorry, Guess again. Too Low")
+        count += 1
         guess = int(input('Enter a number:'))
     elif guess > n:
-        print("Too High")
+        print("Sorry, Guess again. Too High")
+        count += 1
         guess = int(input('Enter a number:'))
     else:
         break
 
-print("You guessed it right!!")
+print('Congrats!! You guessed it right')
+print(f'You guessed it in {count} attempt!!')
