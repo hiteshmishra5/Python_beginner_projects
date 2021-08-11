3cf4531158f2c339759c44a4a04367674a686a2d
Hitesh Mishra <85689306+hiteshmishra5@users.noreply.github.com>
2021-08-11 09:10:03 +0530

    Update Number-Guessing-Game.py

--- Number-Guessing-Game.py
+++ Number-Guessing-Game.py
@@ -1,14 +1,14 @@
 import random
-n = random.randint(10, 100)
+n = random.randint(1, 50)
 count = 0
-print('You have the guess the number in 5 attempt: ')
+print('Note: You have to guess the number in 5 attempts')
 count += 1
-guess = int(input('Enter a number between 1-100: '))
+guess = int(input('Enter a number between 1-50: '))
 while n != guess and count < 5:
     if guess < n:
         print("Sorry, Guess again. Too Low")
         count += 1
-        print(f'You have {6 -count} attempt left')
+        print(f'You have {6 - count} attempt left')
         guess = int(input('Enter a number: '))
     elif guess > n:
         print("Sorry, Guess again. Too High")
@@ -17,11 +17,16 @@ while n != guess and count < 5:
         guess = int(input('Enter a number: '))
     else:
         break
-        
+
 if count == 5 and n != guess:
     print("Sorry, You lost the game")
     print("The number is:", n)
+elif count <= 3:
+    print("You are Truly a Legend")
+    print(f'You guessed it in just {count} attempt')
 else:
     print("You guessed it right!!")
     print(f'You guessed it in just {count} attempt')
-    
+
+
+
