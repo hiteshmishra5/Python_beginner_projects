7b68f593a2d2fff02f50bcc24cb60f6f49cb81c0
Hitesh Mishra <85689306+hiteshmishra5@users.noreply.github.com>
2021-11-10 11:46:56 +0530

    Update Dice_Roll_Simulator.py

--- Dice_Roll_Simulator.py
+++ Dice_Roll_Simulator.py
@@ -6,6 +6,6 @@ while roll_again == 'Yes' or roll_again == 'Y':
     print('Dice Rolling...')
     print(random.randint(min_value, max_value))
     print(random.randint(min_value, max_value))
-    roll_again = input('Roll the Dice Again?').capitalize()
+    roll_again = input('Roll the Dice Again(Yes/Y)?').capitalize()
     if roll_again == 'n'.capitalize():
         break
